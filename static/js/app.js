9fc6ba376f3cb5f2403189efefde7a97a9219112
AliciaAPerez <72763332+AliciaAPerez@users.noreply.github.com>
2021-02-26 08:43:40 -0600

    updating

--- static/js/app.js
+++ static/js/app.js
@@ -11,19 +11,50 @@
 //     console.log(data);
 // })
 
-let data = d3.JSON("../samples.json").then(function(data) {
-    console.log(data);    
+// let data = d3.json("../samples.json", function(data) {
+//     console.log(data);    })
+// //     let names = data.names;
+//     let metadata = data.metadata;
+//     let samples = data.samples;
+//     console.log(names);
+//     console.log(metadata);
+//     console.log(samples);
+// // })
+
+// console.log(data);
+// console.log(names);
+// console.log(metadata);
+// console.log(samples);
+
+
+// let data1 = d3.json("../samples.json", function(data) {
+//     // console.log(data);
+// });
+
+// console.log(data1)
+// let names = data1.names;
+// let metadata = data1.metadata;
+// let samples = data1.samples;
+// console.log(names);
+// console.log(metadata);
+// console.log(samples);
+
+
+// console.log("hello");
+// console.log(data);
+
+
+//import data from json
+d3.json("../samples.json").then(function(data){ 
+    console.log(data)
     let names = data.names;
     let metadata = data.metadata;
     let samples = data.samples;
     console.log(names);
     console.log(metadata);
     console.log(samples);
-}) 
+});
 
-console.log(data);
-console.log(names);
-console.log(metadata);
-console.log(samples);
+//drop down menu
 
-    
+//horizontal bar chart to display top 10 OTUs for text subject
