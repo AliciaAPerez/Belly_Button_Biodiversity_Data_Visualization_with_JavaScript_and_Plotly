37e4d9f7329058a05d2e156542e6a62492a5594f
AliciaAPerez <72763332+AliciaAPerez@users.noreply.github.com>
2021-02-27 09:03:58 -0600

    updating

--- static/js/app.js
+++ static/js/app.js
@@ -1,28 +1,64 @@
 //import data from json
-let data = d3.json("samples.json").then(function(data){ 
-    console.log(data)
-    // let names = data.names;
-    // let metadata = data.metadata;
-    // let samples = data.samples;
-    // console.log(names);
-    // console.log(metadata);
-    // console.log(samples);
+d3.json("samples.json").then(function(data){ 
+    let names = data.names;
+    let metadata = data.metadata;
+    let samples = data.samples;
+    console.log(names);
+    console.log(metadata);
+    console.log(samples);
+
+    //set dropdown menu
+    d3.select("#selDataSet")
+    .selectAll("option")
+    .data(names)
+    .enter()
+    .append("option")
+    .text(function(d) {
+        return `value="${d}`;
+    });
+
+    //default plot
+    function init() {
+        let trace1 = [{
+            x: [],
+            y: []
+            }];
+        let data1 = trace1;
+        //let layout1 = {};
+        Plotly.newPlot("bar", data1);
+        
+        let trace2 = [{
+            domain: {x:[0,1], y:[0,1]},
+            value: 0,
+            type: 'indicator',
+            mode: 'gauge+number',
+            delta: { reference: 400 },
+            gauge: { axis: { range: [null, 500] }}
+            }];
+        let data2 = trace2;
+        let layout2 = { width: 600, height: 500, margin: { t: 0, b: 0 } };
+        Plotly.newPlot("gauge", data2, layout2);
+
+        let trace3 = [{
+            x: [],
+            y: [],
+            mode: 'markers'
+            }];
+        let data3 = trace3;
+        //let layout3 = {};
+        Plotly.newPlot("bubble", data3);
+}
+
+    //turn on event listener
+    // d3.selectAll("#selDataset").on("change", optionChanged);
+
+    //
+
+    init();
+
 });
 
-let names = data.names
 
-console.log(names)
-
-//set dropdown menu
-
-// d3.select("#selDataSet")
-//     .selectAll("option")
-//     .data(names)
-//     .enter()
-//     .append("option")
-//     .text(function(d) {
-//         return `<option value="${d}">${d}</option>`;
-//     });
 // {/* <option value="canada">Canada</option> */}
 // //default plot
 // function init() {
