49161388c53b39dcad93d2bf3484a84f5740277e
AliciaAPerez <72763332+AliciaAPerez@users.noreply.github.com>
2021-02-28 02:50:46 -0600

    updating

--- static/js/app.js
+++ static/js/app.js
@@ -20,46 +20,59 @@ d3.json("samples.json").then(function(data){
     });
 
     //metadata demographic info
-    let demo = d3.select("#sample-metadata")
-        .selectAll("ul")
-        .data(metadata)
-        .enter();
-    demo.append("ul").text(`id: ${metadata[0].id}`);
-    demo.append("ul").text(`ethnicity: ${metadata[0].ethnicity}`);
-    demo.append("ul").text(`gender: ${metadata[0].gender}`);
-    demo.append("ul").text(`age: ${metadata[0].age}`);
-    demo.append("ul").text(`location: ${metadata[0].location}`);
-    demo.append("ul").text(`bbtype: ${metadata[0].bbtype}`);
-    demo.append("ul").text(`wfreq: ${metadata[0].wfreq}`);
+    // let demo = d3.select("#sample-metadata")
+    //     .selectAll("ul")
+    //     .data(metadata)
+    //     .enter();
+    // demo.append("ul").text(`id: ${metadata[0].id}`);
+    // demo.append("ul").text(`ethnicity: ${metadata[0].ethnicity}`);
+    // demo.append("ul").text(`gender: ${metadata[0].gender}`);
+    // demo.append("ul").text(`age: ${metadata[0].age}`);
+    // demo.append("ul").text(`location: ${metadata[0].location}`);
+    // demo.append("ul").text(`bbtype: ${metadata[0].bbtype}`);
+    // demo.append("ul").text(`wfreq: ${metadata[0].wfreq}`);
 
     //default plots
     //bar chart with  top 10 OTUs
-    let topten = samples[0]
-    console.log(topten)
-    let sorttopten = topten.sort(function(a, b) {
-        return b.otu_ids - a.otu_ids;
-    });
-    let slicetopten = sorttopten.slice(0,10)
-    console.log("hi");
-    console.log(sorttopten);
-    console.log(slicetopten);
-    let trace1 = [{
-        x: slicetopten.map(object => object.otu_labels),
-        y: slicetopten.map(object => object.otu_ids)
-        }];
-    let data1 = [trace1];
-    //let layout1 = {};
-    Plotly.newPlot("bar", data1);
+    // let topten = samples[0]
+    // console.log(topten)
+    // let sorttopten = topten.sort(function(a, b) {
+    //     return b.otu_ids - a.otu_ids;
+    // });
+    // let slicetopten = sorttopten.slice(0,10)
+    // console.log("hi");
+    // console.log(sorttopten);
+    // console.log(slicetopten);
+    // let trace1 = [{
+    //     x: slicetopten.map(object => object.otu_labels),
+    //     y: slicetopten.map(object => object.otu_ids)
+    //     }];
+    // let data1 = [trace1];
+    // //let layout1 = {};
+    // Plotly.newPlot("bar", data1);
 
     //bubble chart
-    let trace2 = [{
-        x: [],
-        y: [],
-        mode: 'markers'
-        }];
-    let data2 = trace2;
-    //let layout2 = {};
-    Plotly.newPlot("bubble", data2);
+    console.log(samples[0]);
+    let trace2 = {
+        x: samples[0].otu_ids,
+        y: samples[0].sample_values,
+        text: samples[0].otu_labels,
+        mode: 'markers',
+        marker: {
+            color: samples[0].otu_ids,
+            size: samples[0].sample_values
+        }
+        };
+    let data2 = [trace2];
+    let layout2 = {
+        xaxis: {
+            title: "OTU ID"
+        },
+        showlegend: false,
+        height: 600,
+        width:1000
+    };
+    Plotly.newPlot("bubble", data2, layout2);
 
     //turn on event listener
     //d3.selectAll("#selDataset").on("change", optionChanged);
@@ -76,47 +89,3 @@ d3.json("samples.json").then(function(data){
 
 
 });
-
-
-// // // {/* <option value="canada">Canada</option> */}
-// // // //default plot
-// // // function init() {
-// // //     data = [{
-// // //         x: [1, 2, 3, 4, 5],
-// // //         y: [1, 2, 4, 8, 16] }];
-
-// // //     Plotly.newPlot("plot", data);
-// // // }
-
-// // // // On change to the DOM, call getData()
-// // // d3.selectAll("#selDataset").on("change", optionChanged);
-
-// // // function optionChanged() {
-// // //     let dropdownMenu = d3.select("#selDataset");
-// // //     // Assign the value of the dropdown menu option to a variable
-// // //     let dataset = dropdownMenu.property("value");
-// // //     // Initialize an empty array for the country's data
-// // //     let bardata = [];
-
-// // //     if (dataset == 'us') {
-// // //         data = us;
-// // //     }
-// // //     else if (dataset == 'uk') {
-// // //         data = uk;
-// // //     }
-// // //     else if (dataset == 'canada') {
-// // //         data = canada;
-// // //     }
-// // //     // Call function to update the chart
-// // //     updatePlotly(data);
-// // // }
-
-// // //   // Update the restyled plot's values
-// // //     function updatePlotly(newdata) {
-// // //     Plotly.restyle("pie", "values", [newdata]);
-// // // }
-
-
-// // // //drop down menu
-
-// // // //horizontal bar chart to display top 10 OTUs for text subject
